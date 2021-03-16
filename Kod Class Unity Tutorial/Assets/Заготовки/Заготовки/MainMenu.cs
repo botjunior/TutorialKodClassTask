b0e38050e53cee751e0777edceb11a64303d0392
karnak1221 <karnak1221@gmail.com>
2021-03-16 20:54:44 +0500

    Commit

---
+++ Kod Class Unity Tutorial/Assets/Заготовки/Заготовки/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public Button start;
+    public Button exit;
+    // Update is called once per frame
+    private void Start()
+    {
+        start.onClick.AddListener(StartGame);
+        exit.onClick.AddListener(ExitGame);
+    }
+    void Update()
+    {
+       
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Kod Class Unity Tutorial/Assets/Заготовки/Заготовки/WinScence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScence : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button GoToMainMenu;
+    void Start()
+    {
+        GoToMainMenu.onClick.AddListener(ToMain);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
